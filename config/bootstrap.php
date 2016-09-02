da734fe8635655f039d4922f85a8e2a1ee5562cf
chinpei215 <chinpei215@hotmail.com>
2016-09-02 13:56:24 +0900

    Add support for CakePHP 3.1 or earlier

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,13 @@
 <?php
+use Cake\Core\Configure;
+
 class_alias('Intlless\Number', 'Cake\I18n\Number');
-class_alias('Cake\Chronos\Chronos', 'Cake\I18n\FrozenTime');
-class_alias('Cake\Chronos\Date', 'Cake\I18n\FrozenDate');
-class_alias('Cake\Chronos\MutableDateTime', 'Cake\I18n\Time');
-class_alias('Cake\Chronos\MutableDate', 'Cake\I18n\Date');
+
+if (class_exists('Cake\Chronos\Chronos')) {
+    class_alias('Cake\Chronos\Chronos', 'Cake\I18n\FrozenTime');
+    class_alias('Cake\Chronos\Date', 'Cake\I18n\FrozenDate');
+    class_alias('Cake\Chronos\MutableDateTime', 'Cake\I18n\Time');
+    class_alias('Cake\Chronos\MutableDate', 'Cake\I18n\Date');
+} else {
+    class_alias('Carbon\Carbon', 'Cake\I18n\Time');
+}
